e8490b9caa788f0d6d9ed6c2a2bf95b02f44be68
Jeremy <jeremy.bejanin@gmail.com>
2018-11-01 12:06:31 -0400

    use segmentation instead in a helper function

--- src/helpers.jl
+++ src/helpers.jl
@@ -17,14 +17,14 @@ function list_boards()
     end
 end
 
-function configure_for_ttl_triggering(boardnum,clock=1,channels=3;num_avg=0,avg_len=8192)
+function configure_for_ttl_triggering(boardnum,clock=1,channels=3;num_seg=0,seg_len=8192)
     setup_board(boardnum)
 	set_clock(boardnum,clock)
 	set_channels(boardnum,3) # needed to make sure clock is updated properly when moving to single channel mode
 	set_channels(boardnum,channels)
 	set_trigger(boardnum,4,1)
-	# if num_avg == 0, averaging is disabled
-	set_averager(boardnum,num_avg,avg_len)
+	# if num_seg == 0, segmented capture is disabled
+	set_segmented_capture(boardnum,num_seg,seg_len)
 end
 
 function configure_for_waveform_triggering(boardnum,clock=1,channels=3,trig_ch=1,thresh=2^11,hyst=128;num_avg=0,avg_len=8192)
